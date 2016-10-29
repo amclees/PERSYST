aa9caffe0dcfd6e43e28188682976732bd554803
xv435 <xkx63@aol.com>
2016-10-29 16:19:08 -0700

    Added communications interface.

--- src/centralprocessor/CommunicationsInterface.java
+++ src/centralprocessor/CommunicationsInterface.java
@@ -1,26 +1,157 @@
 package centralprocessor;
-	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
+import java.io.File;
+import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.List;
 
-public class CommunicationsInterface extends Application {
+import org.hive2hive.core.api.interfaces.IFileManager;
+import org.hive2hive.core.exceptions.NoPeerConnectionException;
+import org.hive2hive.core.exceptions.NoSessionException;
+import org.hive2hive.processframework.interfaces.IProcessComponent;
+
+import filemanager.PersistentStorage;
+
+/**
+ * Implementation of communications interface
+ * 
+ * @author Andrew
+ *
+ */
+public class CommunicationsInterface implements ICommunicationsInterface {
+
+	public CommunicationsInterface() {
+
+	}
+
 	@Override
-	public void start(Stage primaryStage) {
+	public boolean login(String username, String password) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void saveConfigurations() {
+		PersistentStorage storage = new PersistentStorage(this.getPassword(),
+				this.getPIN());
+		File file = new File(this.getRootFolder().getAbsolutePath()
+				+ "/.persystconf");
+		file.delete();
+		storage.store(this.getConfigurationsData(), file);
+	}
+
+	@Override
+	public void updateConfigurations() {
+		PersistentStorage storage = new PersistentStorage(this.getPassword(),
+				this.getPIN());
+		byte[] file = storage.read(new File(this.getRootFolder()
+				.getAbsolutePath() + "/.persystconf"));
+		if (file.length == 0)
+			return;
+		// Send signal to user profile
+	}
+
+	@Override
+	public void sendShutdownSignal() {
+		this.updateConfigurations();
+		this.saveConfigurations();
+		System.exit(0);
+	}
+
+	@Override
+	public Serializable getConfiguration(String configuration) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setConfiguration(String configuration, Serializable value) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public byte[] getConfigurationsData() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPIN() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IProcessComponent<Void> createDownloadProcess(File file) {
+		// Code for this session
+		return null;
+	}
+
+	@Override
+	public IProcessComponent<Void> createDownloadProcess(File file,
+			IFileManager filemanager) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
+			return filemanager.createDownloadProcess(file);
+		} catch (NoPeerConnectionException e) {
+			e.printStackTrace();
+		} catch (NoSessionException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
 			e.printStackTrace();
 		}
+		return null;
 	}
-	
-	public static void main(String[] args) {
-		launch(args);
+
+	@Override
+	public void storeData(byte[] data, File location) {
+		PersistentStorage storage = new PersistentStorage(this.getPassword(),
+				this.getPIN());
+		storage.store(data, location);
 	}
+
+	@Override
+	public byte[] readData(File location) {
+		PersistentStorage storage = new PersistentStorage(this.getPassword(),
+				this.getPIN());
+		return storage.read(location);
+	}
+
+	@Override
+	public Serializable fromBytes(byte[] data) {
+		return PersistentStorage.fromBytes(data);
+	}
+
+	@Override
+	public byte[] toBytes(Serializable object) {
+		return PersistentStorage.toBytes(object);
+	}
+
+	@Override
+	public boolean setRootFolder(File rootFolder) {
+		try {
+			this.setConfiguration("rootfolder", rootFolder);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	@Override
+	public File getRootFolder() {
+		return (File) this.getConfiguration("rootfolder");
+	}
+
 }
---
+++ src/centralprocessor/ICommunicationsInterface.java
@@ -0,0 +1,109 @@
+package centralprocessor;
+import java.io.File;
+import java.io.Serializable;
+
+import org.hive2hive.core.api.interfaces.IFileManager;
+import org.hive2hive.processframework.interfaces.IProcessComponent;
+
+/**
+ * IMPORTANT: If you need to modify this once it is in GitHub, do so in the
+ * Github editor
+ * 
+ * This is the interface for the Communication Interface submodule. This should
+ * contain all public methods for communication between modules.
+ * 
+ * TODO Write a concrete implementation TODO Write a static register for this
+ */
+public interface ICommunicationsInterface {
+	
+	public boolean login(String username, String password);
+	
+	public void saveConfigurations();
+
+	public void updateConfigurations();
+
+	public void sendShutdownSignal();
+
+	public Serializable getConfiguration(String configuration);
+
+	public void setConfiguration(String configuration, Serializable value);
+
+	public byte[] getConfigurationsData();
+
+	public String getPassword();
+
+	public String getUsername();
+
+	public String getPIN();
+
+	/**
+	 * This method is needed if we want to wrap download processes. It needs the
+	 * IFileManager (H2H) if this interface does not have a field for it. If we
+	 * use this, we need to modify the wrapper in FileEventListener
+	 */
+	public IProcessComponent<Void> createDownloadProcess(File file);
+
+	public IProcessComponent<Void> createDownloadProcess(File file,
+			IFileManager filemanager);
+
+	/**
+	 * This method sends data to the File Manaager to be encrypted and stored
+	 *
+	 * @param data
+	 *            Data to be stored
+	 * @param location
+	 *            Location to store the data
+	 */
+	public void storeData(byte[] data, File location);
+
+	/**
+	 * This method sends a file path to the File Manager to be decrypted and
+	 * read
+	 *
+	 * @param location
+	 *            Location to read
+	 * @return Data read, or empty if the file is not stored using
+	 *         PersistentStorage
+	 */
+	public byte[] readData(File location);
+
+	/**
+	 * These method reads an object from a byte array.
+	 *
+	 * @param data
+	 *            The byte array containing the object
+	 * @return The object from the data. Null if the data is not a valid object.
+	 */
+	public Serializable fromBytes(byte[] data);
+
+	/**
+	 * These method creates a byte array from an object.
+	 *
+	 * @param object
+	 *            A serializable object to be converted to bytes
+	 * @return The byte array corresponding to the object
+	 */
+	public byte[] toBytes(Serializable object);
+
+	/*
+	 * 
+	 * GUI SECTION
+	 */
+
+	/**
+	 * These method creates and sets the root folder.
+	 *
+	 * @param File
+	 *            with with the path of the root folder
+	 * @return true if the File is a directory or the directory was created
+	 */
+	public boolean setRootFolder(File rootFolder);
+
+	/**
+	 * These method returns the root folder as a File.
+	 *
+	 * @return the root folder as a File
+	 */
+	public File getRootFolder();
+
+}
