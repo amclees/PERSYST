2e426f67fc9a39091ce1262a5e21ddc625613e20
xv435 <xv435@users.noreply.github.com>
2016-11-04 10:04:10 -0700

    Merged Configurations change from Wilson's branch

--- src/centralprocessor/PERSYSTSession.java
+++ src/centralprocessor/PERSYSTSession.java
@@ -5,4 +5,5 @@ import userprofile.UserProfile;
 public class PERSYSTSession {
 	public static ICommunicationsInterface comm;
 	public static UserProfile usr; //Initialize in Central Processor
+	public static FileConfiguration confi = new FileConfiguration();
 }
