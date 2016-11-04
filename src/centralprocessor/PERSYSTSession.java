6dbc4ad0c5289b368ff1e29bc604bb44f3f23c50
wthomas0293 <wthomas0293@gmail.com>
2016-11-03 23:00:17 -0700

    Update PERSYSTSession.java
    passing FileConfiguration

--- src/centralprocessor/PERSYSTSession.java
+++ src/centralprocessor/PERSYSTSession.java
@@ -1,5 +1,15 @@
+
+
 package centralprocessor;
+
+import org.hive2hive.core.api.configs.FileConfiguration;
 
 public class PERSYSTSession {
 	public static ICommunicationsInterface comm = new CommunicationsInterface();
+	
+	public static FileConfiguration confi = new FileConfiguration();
+	
+	
+	
 }
+
